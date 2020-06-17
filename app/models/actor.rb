e2ff29294e78550509c6fe3f0500409277ee313e
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-17 01:18:17 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    first_name = :first_name
+    last_name = :last_name
+    "#{first_name} #{last_name}}"
+  end
+  
+  def list_roles
+    self.characters.collect do |t|
+      "#{t.name} - #{t.show}"
+    end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  def actors_list
+    self.actors.map do |t|
+      t.full_name
+    end
+  end
 end
